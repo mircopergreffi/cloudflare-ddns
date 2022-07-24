719fd440503df9a3d96085c08fcf5b8c0ef10503
Mirco Pergreffi <mircopergreffi@gmail.com>
2022-07-24 22:30:25 +0200

    Update

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 
-import yaml
-import requests
-import re
-import CloudFlare
+import time, yaml, requests, re, CloudFlare
+
+def get_ipaddr():
+    content = requests.get('http://192.168.1.1/network-expert-internet.lp?ip=&phoneType=undefined').content.decode('utf-8')
+    content = content.replace(' ', '')
+    content = content.replace('\n', '')
+    content = content.replace('\r', '')
+    return re.search('<dt>IndirizzoIPv4</dt><dd>([0-9.]+)</dd>', content).group(1)
+
+def update_dns(cfg, ipaddr):
+    cf = CloudFlare.CloudFlare(email=cfg['cloudflare']['email'], token=cfg['cloudflare']['token'])
+    for entry in cfg['update_ip']:
+        dnsrecord = cf.zones.dns_records.get(entry['zone_id'], entry['id'])
+        data = {'type':dnsrecord['type'], 'name':dnsrecord['name'], 'content':ipaddr, 'ttl':dnsrecord['ttl'], 'proxied':dnsrecord['proxied']}
+        cf.zones.dns_records.put(entry['zone_id'], entry['id'], data=data)
 
 if __name__ == '__main__':
 
@@ -14,15 +25,10 @@ if __name__ == '__main__':
             print('Error: Could not load config file')
             exit(1)
 
-    # Get IP address
-    content = requests.get('http://192.168.1.1/network-expert-internet.lp?ip=&phoneType=undefined').content.decode('utf-8')
-    content = content.replace(' ', '')
-    content = content.replace('\n', '')
-    content = content.replace('\r', '')
-    ipaddr = re.search('<dt>IndirizzoIPv4</dt><dd>([0-9.]+)</dd>', content).group(1)
-    
-    cf = CloudFlare.CloudFlare(email=cfg['cloudflare']['email'], token=cfg['cloudflare']['token'])
-    for entry in cfg['update_ip']:
-        dnsrecord = cf.zones.dns_records.get(entry['zone_id'], entry['id'])
-        data = {'type':dnsrecord['type'], 'name':dnsrecord['name'], 'content':ipaddr, 'ttl':dnsrecord['ttl'], 'proxied':dnsrecord['proxied']}
-        cf.zones.dns_records.put(entry['zone_id'], entry['id'], data=data)
+    current_ip = ''
+    while True:
+        new_ip = get_ipaddr()
+        if new_ip != current_ip:
+            update_dns(cfg, new_ip)
+            current_ip = new_ip'
+        time.sleep(cfg['update_interval'])
